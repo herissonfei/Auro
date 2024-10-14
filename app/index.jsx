24b3c815482f36c3e4ecbb824da90e37b6c86c8d
Hangfei Lin <1023584582@qq.com>
2024-10-13 22:17:05 -0400

    tabs finish

--- app/index.jsx
+++ app/index.jsx
@@ -1,22 +1,22 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text } from "react-native";
 import { Link } from "expo-router";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Auro</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl font-pblack">Auro!</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: "blue" }}>
-        go to profile
+      <Link href="/home" style={{ color: "blue" }}>
+        go to Home
       </Link>
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#fff",
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+// });
